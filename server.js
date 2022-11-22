a91008aa144ad55c4133e510af881f4c842cc026
Tyler-Will <tylerceeme@gmail.com>
2022-11-22 12:37:31 -0800

    sending index and show data

--- server.js
+++ server.js
@@ -6,6 +6,14 @@ require('dotenv').config()
 const PORT = process.env.PORT
 const app = express()
 
+// MIDDLEWARE
+app.set('views', __dirname + '/views')
+app.set('view engine', 'jsx')
+app.engine('jsx', require('express-react-views').createEngine())
+app.use(express.static('public'))
+
+
+
 // ROUTES
 app.get('/', (req, res) => {
     res.send('Welcome to an Awesome App about Breads')
@@ -19,4 +27,9 @@ app.get('/', (req, res) => {
 // LISTEN
 app.listen(PORT, () => {
   console.log('listening on port', PORT);
+})
+
+// 404 Page
+app.get('*', (req, res) => {
+  res.send('404')
 })
