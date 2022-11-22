b4b15404615ea48b7c5de820c17a9c97ed334a24
Tyler-Will <tylerceeme@gmail.com>
2022-11-22 13:08:00 -0800

    delete route complete

--- controllers/breads_controller.js
+++ controllers/breads_controller.js
@@ -16,6 +16,12 @@ breads.post('/', (req, res) => {
   res.redirect('/breads')
 })
 
+// DELETE
+breads.delete('/:indexArray', (req, res) => {
+  Bread.splice(req.params.indexArray, 1)
+  res.status(303).redirect('/breads')
+})
+
 
 // INDEX
 breads.get('/', (req, res) => {
@@ -36,10 +42,11 @@ breads.get('/new', (req, res) => {
 breads.get('/:arrayIndex', (req, res) => {
   if (Bread[req.params.arrayIndex]) {
     res.render('Show', {
-      bread:Bread[req.params.arrayIndex]
+      bread:Bread[req.params.arrayIndex],
+      index: req.params.arrayIndex,
     })
   } else {
-    res.send('404')
+    res.render('404')
   }
 })
 
